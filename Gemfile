746c0abe447bf358cca2449c66a7effd94e6e232
Ben <bjlinville1@gmail.com>
2016-01-21 12:01:46 -0500

    added & configured database cleaner, fixed logic so todo list is scoped to user

--- Gemfile
+++ Gemfile
@@ -19,4 +19,5 @@ end
 
 group :test do
   gem 'capybara', '~> 2.3.0'
+  gem 'database_cleaner' 
 end
--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -4,11 +4,9 @@ feature "User creates to do" do
   scenario "successfully" do
     sign_in 
     click_on "Add a new todo"
-    fill_in "Title", with: "Buy bread"
+    fill_in "Title", with: "Buy milk"
     click_on "Submit"
-    # binding.pry
 
     expect(page).to have_css '.todos li', text: "Buy milk"
-    #this is passing. Buy bread and Buy milk are clearly different text. what am i missing? Its not checking that text = "Buy milk"
   end
 end
