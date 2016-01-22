c87b730c47665ce5f2779224bdbd4003bc95a470
Ben <bjlinville1@gmail.com>
2016-01-22 13:48:18 -0500

    refactored user logic and added user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,15 @@ class ApplicationController < ActionController::Base
   end
 
   def signed_in?
-    current_email.present?
-  end
-
-  def current_email
-    session[:current_email]    
+    current_user.signed_in?
+    #because calling .signed_in? within the signed_in? method is more clear? I disagree with Josh here, but whatevs
   end
 
   def sign_in_as email
     session[:current_email] = email
+  end
+
+  def current_user
+    User.new(session[:current_email])
   end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,9 @@
 class TodosController < ApplicationController  
   before_filter :authenticate
 
+
   def index
-    @todos = Todo.where(email: current_email)
+    @todos = current_user.todos
   end
 
   def new
@@ -10,7 +11,7 @@ class TodosController < ApplicationController
   end
   
   def create
-    Todo.create(todo_params.merge(email: current_email))
+    current_user.todos.create(todo_params)
     redirect_to todos_path
   end
 
