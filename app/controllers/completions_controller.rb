28b4a3ff9db2d16da28cab6252850bcd1d9dc293
Ben <bjlinville1@gmail.com>
2016-01-26 17:09:24 -0500

    extracted todo completion logic from the controller to the model, and updated tests to reflect it

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,6 +1,6 @@
 class CompletionsController < ApplicationController
   def create
-    todo.touch :completed_at #touch sets a timestamp to the current time then persists the record. Plenty to refactor, but feature works, and the suite is green, so we're committing
+    todo.complete! #touch sets a timestamp to the current time then persists the record. Plenty to refactor, but feature works, and the suite is green, so we're committing
     redirect_to todos_path
   end
 
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -13,3 +13,15 @@ describe Todo, "#completed?" do
     expect(todo).not_to be_completed
   end
 end
+
+describe Todo, "#complete!" do
+  it "update completed at" do
+    todo = Todo.create!(completed_at: nil)
+
+    todo.complete!
+
+    todo.reload
+
+    expect(todo).to be_completed
+  end
+end
